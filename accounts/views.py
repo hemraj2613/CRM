8423b773983345d055d44f49c0c052fa297d934e
hemraj11 <hemrajojha1122334455@gmail.com>
2026-09-12 20:36:58 +0545

    CRM system

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+
+def customer(request):
+    return render(request, 'customer.html')
