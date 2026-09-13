6c490824b3332982616db2d6352869b0077a5a3d
hemraj11 <hemrajojha1122334455@gmail.com>
2026-09-13 14:54:44 +0545

    data fetched from database and rendered to templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render
+from .models import Product, Customer, Order
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    customers = Customer.objects.all()
+    orders= Order.objects.all()
+
+    total_orders = orders.count()
+
+    total_customers = customers.count()
+
+    orders_delivered = orders.filter(status='Delivered').count()
+    orders_pending = orders.filter(status='Pending').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_orders': total_orders,
+        'total_customers': total_customers,
+        'orders_delivered': orders_delivered,
+        'orders_pending': orders_pending,
+    }
+    return render(request, 'home.html', context)
 
 def product(request):
-    return render(request, 'product.html')
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'product.html', context)
 
 
-def customer(request):
-    return render(request, 'customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    total_orders = orders.count()
+
+    context = {
+        'customer': customer,
+        'orders': orders,
+        'total_orders':total_orders,
+    }
+    return render(request, 'customer.html',context)
